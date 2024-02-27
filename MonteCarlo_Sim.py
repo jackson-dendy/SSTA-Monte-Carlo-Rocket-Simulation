7571becb992b227a0af165599ddcb6c9d6d765d9
Jackson Dendy <146208057+jackson-dendy@users.noreply.github.com>
2024-02-27 12:31:17 -0500

    modified:   MonteCarlo_Sim.py 	modified:   Tools.py

--- MonteCarlo_Sim.py
+++ MonteCarlo_Sim.py
@@ -4,9 +4,10 @@ from numpy.random import normal, choice
 import xlsxwriter
 import sys
 import Tools
-from Tools import heading_finder
+from Tools import heading_finder, log_dec
 from Environment_Analysis import data_collection, iterator
 from Wind_Data import multipro
+from rocketpy.plots.flight_plots import _FlightPlots
 ####################################################################################
 # This code is a Monte Carlo Simulation for the SSTA Big Liquid program
 # To run this code more sure to install the rocketpy, numpy, and xlsxwriter ibraries
@@ -18,14 +19,14 @@ from Wind_Data import multipro
 def main():
     # Basic Parameters of the Simulation below
     ###############################
-    number_of_simulations = 3
+    number_of_simulations = 400
     ###############################
     years = [2021, 2022, 2020, 2018, 2019, 2017]
     ###############################
     max_height = 25000 # in meters
     ###############################
     # To change Collection date go to Wind_Data and change parameters on the Wind_Data1 function
-    multipro(years)
+    #multipro(years)
     cov_x, cov_y, cov_temp, cov_pressure, mean_x, mean_y, mean_temp, mean_pressure, altitude = data_collection(years, max_height)
     simulation(number_of_simulations, cov_x, cov_y, cov_temp, mean_x, mean_y, mean_temp, altitude, max_height)
     
@@ -295,9 +296,16 @@ def simulation(num_sim,cov_x, cov_y, cov_temp, mean_x, mean_y, mean_temp, altitu
             heading=setting["heading"],
             max_time_step=0.01,
             terminate_on_apogee=False,
-            max_time = 5000
+            max_time = 4
 
         )
+
+        aoa = flight_data.angle_of_attack
+        num1 = aoa.set_discrete(1.75, 4, mutate_self= False)
+        first_max = num1.max
+        num2 = aoa.set_discrete(2.75, 4, mutate_self = False)
+        second_max = num2.max
+        damp_ratio = log_dec(first_max, second_max)
 
         # Selected Returned Values for the flight (can add more)
         flight_result = {
@@ -319,7 +327,8 @@ def simulation(num_sim,cov_x, cov_y, cov_temp, mean_x, mean_y, mean_temp, altitu
             "final_static_margin": flight_data.rocket.static_margin(
                 flight_data.rocket.motor.burn_out_time
             ),
-            "number_of_events": len(flight_data.parachute_events)
+            "number_of_events": len(flight_data.parachute_events),
+            "Damping_Ratio": damp_ratio
         }
 
         # writes flight result titles to the output file for the first iteration
